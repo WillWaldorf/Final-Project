10ed5fec79751a99d7d7acf21d6beba00b1e81b5
Will Waldorf <02william.waldorf@gulllakecs.org>
2019-03-12 09:53:54 -0400

    Add For Loops

--- Final.Project.py
+++ Final.Project.py
@@ -12,14 +12,19 @@ def greeting():
 greeting()
 
 # Asking amount of games that are wanted to be averaged
-total = 0
-totals = 0
 
-how_many_games = int(input("How many games would you like to average?: "))
+try:
+    how_many_games = int(input("Enter how many games you would like to average?: "))
+    print('Your amount of games you would like to average: ', str(how_many_games))
+
+except ValueError:
+    print('That was not a proper number')
 
 print("")
 
 # Asking amount of shots wanted to be averaged
+total = 0
+totals = 0
 for i in range(how_many_games):
     number_of_shots = int(input('\n''How many shots did you take in this game?: '))
     number_of_made_shots = int(input('\n''How many shots did you make in this game?: '))
@@ -32,10 +37,10 @@ print("")
 average = total / totals
 print('\n')
 
+x = total
+
+
 print ('Shot Percentage: ' + str(round(average, 2)))
 
 
-print('Your average was', average, 'goals per game' + str(round(average, 2))
-
-
-
+print('The average number of shots you made was ', str(round(average, 2)), 'goals per game')
