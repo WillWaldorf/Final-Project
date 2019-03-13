a6750e23ab7a27471ca66d148a18f0787b35fc3e
WillWaldorf <48022401+WillWaldorf@users.noreply.github.com>
2019-03-13 10:00:38 -0400

    Add for i in range

--- Final.Project.py
+++ Final.Project.py
@@ -1,15 +1,12 @@
-# Will Waldorf
-# Python Final Project
-# Average of soccer shots
+# Asking amount of shots wanted to be averaged
+total = 0
+totals = 0
+for i in range(how_many_games):
+    number_of_shots = int(input('\n''How many shots did you take in this game?: '))
+    number_of_made_shots = int(input('\n''How many shots did you make in this game?: '))
+    total = total + number_of_shots
+    totals = totals + number_of_made_shots
 
-#Greeting
-name = input("What is your name: ")
-
-def greeting():
-    print("Hi there " + name + "!")
-    print ('Its nice to meet you ! ')
-
-greeting()
-
+print("")
 
 
